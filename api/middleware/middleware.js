52bbf6d45442f7afd7f41d58cc18a944a0d930cb
Winifred Lu <winnie.lu@comcast.net>
2022-02-09 16:21:51 -0500

    get user by id functional

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -15,6 +15,7 @@ function validateUserId(req, res, next) {
       if(!user){
         res.status(404).json({message: 'user not found'});
       } else {
+        req.user = user;
         next();
       }
     })
