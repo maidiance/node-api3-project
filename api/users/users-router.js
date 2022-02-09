60adaf941a2c1c7f17901fc59e824ecf7c715045
Winifred Lu <winnie.lu@comcast.net>
2022-02-09 14:54:22 -0500

    server required and running properly

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const { validateUserId, validateUser, validatePost } = require('../middleware/middleware');
+
 // You will need `users-model.js` and `posts-model.js` both
+const Users = require('./users-model');
+const Posts = require('../posts/posts-model');
 // The middleware functions also need to be required
+const { validateUserId, validateUser, validatePost } = require('../middleware/middleware');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res.status(500).json({message: 'Error retrieving the users'});
+    })
 });
 
 router.get('/:id', validateUserId, (req, res) => {
